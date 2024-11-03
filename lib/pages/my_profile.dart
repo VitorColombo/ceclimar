7deddd41d1a4e0075f8caeeab5aa8ff8d598bca3
VitorColombo <vitorcolombonunes@gmail.com>
2024-11-03 03:28:12 -0300

    feat: adicionando lista de ultimos registros na tela de perfil

--- lib/pages/my_profile.dart
+++ lib/pages/my_profile.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:tcc_ceclimar/controller/auth_user_controller.dart';
+import 'package:tcc_ceclimar/models/register_response.dart';
 import 'package:tcc_ceclimar/widgets/header_banner_widget.dart';
+import 'package:tcc_ceclimar/widgets/register_item.dart';
 import '../models/user_data.dart';
 import '../widgets/page_header.dart';
 import '../widgets/profile_switch.dart';
@@ -19,11 +22,189 @@ class MyProfile extends StatefulWidget {
 
 class _MyProfileState extends State<MyProfile> {
   final AuthenticationController _controller = AuthenticationController();
-  bool isUltimosRegistros = true;
+  final ValueNotifier<bool> isUltimosRegistrosNotifier = ValueNotifier<bool>(false);
+  bool isLoading = true;
+  List<RegisterResponse> registers = [];
 
   void _logout(BuildContext context) {
     _controller.signOut();
     Navigator.pushReplacementNamed(context, '/login');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMockedRegisters();
+  }
+  
+  Future<void> fetchMockedRegisters() async { //todo remover mocks
+    await Future.delayed(const Duration(milliseconds: 2000)); 
+    if (!mounted) return;
+    setState(() {
+      registers = [
+        RegisterResponse(
+          uid: '1',
+          date: '20/10/2020',
+          popularName: 'Lobo Marinho',
+          city: 'Xangri-lá',
+          state: true,
+          authorName: 'John Doe',
+          species: 'Otaria flavescens',
+          location: const GeoPoint(-30.0345, -50.6452),
+          registerImage: Image.asset(
+            'assets/images/logo.png',
+            fit: BoxFit.cover
+          )
+        ),
+        RegisterResponse(
+          uid: '2',
+          date: '25/10/2020',
+          popularName: 'Pinguim-de-magalhaes',
+          city: 'Xangri-lá',
+          state: false,
+          species: 'Spheniscus magellanicus',
+          location: const GeoPoint(-30.0345, -50.6452),
+          registerImage: Image.asset(
+            'assets/images/logo.png',
+            fit: BoxFit.cover
+          )
+        ),
+        RegisterResponse(
+          uid: '3',
+          date: '30/10/2020',
+          popularName: 'Pinguim-de-magalhaes',
+          city: 'Xangri-lá',
+          state: true,
+          species: 'Spheniscus magellanicus',
+          location: const GeoPoint(-30.0345, -50.6452),
+          registerImage: Image.asset(
+            'assets/images/logo.png',
+            fit: BoxFit.cover
+          )
+        ),
+        RegisterResponse(
+          uid: '1',
+          date: '20/10/2020',
+          popularName: 'Lobo Marinho',
+          city: 'Xangri-lá',
+          state: true,
+          authorName: 'John Doe',
+          species: 'Otaria flavescens',
+          location: const GeoPoint(-30.0345, -50.6452),
+          registerImage: Image.asset(
+            'assets/images/logo.png',
+            fit: BoxFit.cover
+          )
+        ),
+        RegisterResponse(
+          uid: '2',
+          date: '25/10/2020',
+          popularName: 'Pinguim-de-magalhaes',
+          city: 'Xangri-lá',
+          state: false,
+          species: 'Spheniscus magellanicus',
+          location: const GeoPoint(-30.0345, -50.6452),
+          registerImage: Image.asset(
+            'assets/images/logo.png',
+            fit: BoxFit.cover
+          )
+        ),
+        RegisterResponse(
+          uid: '3',
+          date: '30/10/2020',
+          popularName: 'Pinguim-de-magalhaes',
+          city: 'Xangri-lá',
+          state: true,
+          species: 'Spheniscus magellanicus',
+          location: const GeoPoint(-30.0345, -50.6452),
+          registerImage: Image.asset(
+            'assets/images/logo.png',
+            fit: BoxFit.cover
+          )
+        ),
+        RegisterResponse(
+          uid: '1',
+          date: '20/10/2020',
+          popularName: 'Lobo Marinho',
+          city: 'Xangri-lá',
+          state: true,
+          authorName: 'John Doe',
+          species: 'Otaria flavescens',
+          location: const GeoPoint(-30.0345, -50.6452),
+          registerImage: Image.asset(
+            'assets/images/logo.png',
+            fit: BoxFit.cover
+          )
+        ),
+        RegisterResponse(
+          uid: '2',
+          date: '25/10/2020',
+          popularName: 'Pinguim-de-magalhaes',
+          city: 'Xangri-lá',
+          state: false,
+          species: 'Spheniscus magellanicus',
+          location: const GeoPoint(-30.0345, -50.6452),
+          registerImage: Image.asset(
+            'assets/images/logo.png',
+            fit: BoxFit.cover
+          )
+        ),
+        RegisterResponse(
+          uid: '3',
+          date: '30/10/2020',
+          popularName: 'Pinguim-de-magalhaes',
+          city: 'Xangri-lá',
+          state: true,
+          species: 'Spheniscus magellanicus',
+          location: const GeoPoint(-30.0345, -50.6452),
+          registerImage: Image.asset(
+            'assets/images/logo.png',
+            fit: BoxFit.cover
+          )
+        ),
+        RegisterResponse(
+          uid: '1',
+          date: '20/10/2020',
+          popularName: 'Lobo Marinho',
+          city: 'Xangri-lá',
+          state: true,
+          authorName: 'John Doe',
+          species: 'Otaria flavescens',
+          location: const GeoPoint(-30.0345, -50.6452),
+          registerImage: Image.asset(
+            'assets/images/logo.png',
+            fit: BoxFit.cover
+          )
+        ),
+        RegisterResponse(
+          uid: '2',
+          date: '25/10/2020',
+          popularName: 'Pinguim-de-magalhaes',
+          city: 'Xangri-lá',
+          state: false,
+          species: 'Spheniscus magellanicus',
+          location: const GeoPoint(-30.0345, -50.6452),
+          registerImage: Image.asset(
+            'assets/images/logo.png',
+            fit: BoxFit.cover
+          )
+        ),
+        RegisterResponse(
+          uid: '3',
+          date: '30/10/2020',
+          popularName: 'Pinguim-de-magalhaes',
+          city: 'Xangri-lá',
+          state: true,
+          species: 'Spheniscus magellanicus',
+          location: const GeoPoint(-30.0345, -50.6452),
+          registerImage: Image.asset(
+            'assets/images/logo.png',
+            fit: BoxFit.cover
+          )
+        ),
+      ];
+      isLoading = false;
+    });
   }
 
   @override
@@ -34,6 +215,8 @@ class _MyProfileState extends State<MyProfile> {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            pinned: true,
+            collapsedHeight: 280,
             expandedHeight: 280,
             backgroundColor: Colors.white,
             shadowColor: Color.fromARGB(0, 173, 145, 145),
@@ -59,21 +242,47 @@ class _MyProfileState extends State<MyProfile> {
               ),
             ),
           ),
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                Text(
-                  '${userData?.name}',
-                  style: Theme.of(context).textTheme.titleLarge,
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Column(
+                  children: [
+                    Text(
+                      '${userData?.name}',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    SizedBox(height: 9),
+                    Text(
+                      "Registros realizados: 123", //todo integração
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                    SizedBox(height: 20),
+                    ProfileSwitch(
+                        size: 600,
+                        isUltimosRegistrosNotifier: isUltimosRegistrosNotifier),
+                    SizedBox(height: 10),
+                  ],
                 ),
-                SizedBox(height: 9),
-                Text(
-                  "Registros realizados: 123",//todo integração
-                  style: Theme.of(context).textTheme.bodyLarge,
+                ValueListenableBuilder<bool>(
+                  valueListenable: isUltimosRegistrosNotifier,
+                  builder: (context, isUltimosRegistros, child) {
+                    return isLoading
+                        ? Padding(
+                          padding: const EdgeInsets.all(40.0),
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: const LinearProgressIndicator(
+                                color: Color.fromRGBO(71, 169, 218, 1), 
+                                backgroundColor: Color.fromARGB(255, 31, 73, 95),
+                                minHeight: 4,
+                              ),
+                          ),
+                        )
+                        : !isUltimosRegistros
+                            ? UltimosRegistrosContent(registers: registers)
+                            : AnimaisEncontradosContent();
+                  },
                 ),
-                SizedBox(height: 20),
-                ProfileSwitch(size: 600),
-                SizedBox(height: 20),
               ]
             )
           )
@@ -84,31 +293,24 @@ class _MyProfileState extends State<MyProfile> {
 }
 
 class UltimosRegistrosContent extends StatelessWidget {
+  final List<RegisterResponse> registers;
+
+  const UltimosRegistrosContent({super.key, required this.registers});
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ListTile(
-          title: Text('Pinguim de Magalhães'),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Xangri-lá'),
-              Text('Enviado em 20/10/2020'),
-            ],
-          ),
-        ),
-        ListTile(
-          title: Text('Lobo Marinho'),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Imbé'),
-              Text('Enviado em 20/10/2020'),
-            ],
-          ),
-        ),
-      ],
+    final limitedRegisters = registers.take(10).toList();
+
+    return SizedBox(
+      height: 400,
+      child: ListView.builder(
+        padding: EdgeInsets.only(top: 0, bottom: 70),
+        physics: AlwaysScrollableScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: limitedRegisters.length,
+        itemBuilder: (context, index) {
+          return RegisterItem(register: limitedRegisters[index]);
+        },
+      ),
     );
   }
 }
--- lib/widgets/profile_switch.dart
+++ lib/widgets/profile_switch.dart
@@ -2,87 +2,98 @@ import 'package:flutter/material.dart';
 
 class ProfileSwitch extends StatefulWidget {
   final double size;
-  const ProfileSwitch({super.key, required this.size});
+  final ValueNotifier<bool> isUltimosRegistrosNotifier;
+
+  const ProfileSwitch({super.key, required this.size, required this.isUltimosRegistrosNotifier});
   
   @override
   State<ProfileSwitch> createState() => _ProfileSwitchState();
 }
 
 class _ProfileSwitchState extends State<ProfileSwitch> {
-  bool foundAnimalsChecked = false;
-  bool lastRegistersChecked = true;
+  bool get foundAnimalsChecked => !widget.isUltimosRegistrosNotifier.value;
+  bool get lastRegistersChecked => widget.isUltimosRegistrosNotifier.value;
+  set foundAnimalsChecked(bool value) => widget.isUltimosRegistrosNotifier.value = value;
+  set lastRegistersChecked(bool value) => widget.isUltimosRegistrosNotifier.value = value;
 
   @override
   Widget build(BuildContext context) {
     return
-        Container(
-          width: 360,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Color.fromARGB(255, 232, 232, 232)),
-          child: Padding(
-            padding: const EdgeInsets.only(top: 2, bottom: 2, left: 2, right: 2),
-            child: Row(
-              mainAxisAlignment:
-                  MainAxisAlignment.spaceBetween,
-              children: [
-                GestureDetector(
-                  onTap: () => {
-                    setState(() {
-                      lastRegistersChecked = false;
-                      foundAnimalsChecked = true;
-                    })
-                  },
-                  child: Container(
-                    width: 170,
-                    height: 50,
-                    decoration: BoxDecoration(
-                        borderRadius:
-                            BorderRadius.circular(30),
-                        color: foundAnimalsChecked
-                            ? Colors.white
-                            : Color.fromARGB(255, 232, 232, 232)),
-                    child: Center(
-                        child: Text(
-                      'Últimos registros',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: foundAnimalsChecked
-                              ? Color.fromARGB(255, 0, 111, 130)
-                              : Colors.white),
-                    )),
-                  ),
+        ValueListenableBuilder<bool>(
+          valueListenable: widget.isUltimosRegistrosNotifier,
+          builder: (context, value, child) {
+            return Container(
+              width: 360,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Color.fromARGB(255, 232, 232, 232)),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 2, bottom: 2, left: 2, right: 2),
+                child: Row(
+                  mainAxisAlignment:
+                      MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      onTap: () => {
+                        setState(() {
+                          lastRegistersChecked = true;
+                          foundAnimalsChecked = false;
+                          widget.isUltimosRegistrosNotifier.value = lastRegistersChecked;
+                        })
+                      },
+                      child: Container(
+                        width: 170,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.circular(30),
+                            color: foundAnimalsChecked
+                                ? Colors.white
+                                : Color.fromARGB(255, 232, 232, 232)),
+                        child: Center(
+                            child: Text(
+                          'Últimos registros',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: foundAnimalsChecked
+                                  ? Color.fromARGB(255, 0, 111, 130)
+                                  : Colors.white),
+                        )),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () => {
+                        setState(() {
+                          lastRegistersChecked = false;
+                          foundAnimalsChecked = true;
+                          widget.isUltimosRegistrosNotifier.value = lastRegistersChecked;
+                        })
+                      },
+                      child: Container(
+                        width: 180,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.circular(30),
+                            color: lastRegistersChecked
+                                ? Colors.white
+                                : Color.fromARGB(255, 232, 232, 232)),
+                        child: Center(
+                            child: Text(
+                          'Animais encontrados',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: lastRegistersChecked
+                                  ? Color.fromARGB(255, 0, 111, 130)
+                                  : Color.fromARGB(255, 189, 189, 189)),
+                        )),
+                      ),
+                    ),
+                  ],
                 ),
-                GestureDetector(
-                  onTap: () => {
-                    setState(() {
-                      lastRegistersChecked = true;
-                      foundAnimalsChecked = false;
-                    })
-                  },
-                  child: Container(
-                    width: 180,
-                    height: 50,
-                    decoration: BoxDecoration(
-                        borderRadius:
-                            BorderRadius.circular(30),
-                        color: foundAnimalsChecked
-                            ? Color.fromARGB(255, 232, 232, 232)
-                            : Colors.white),
-                    child: Center(
-                        child: Text(
-                      'Animais encontrados',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: foundAnimalsChecked
-                              ? Color.fromARGB(255, 189, 189, 189)
-                              : Color.fromARGB(255, 0, 111, 130)),
-                    )),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
+              ),
+          );
+      },
+   );
   }
 }
