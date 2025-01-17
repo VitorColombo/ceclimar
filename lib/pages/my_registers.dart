295c7c22fc4e0690a7767f48a4b7173dd49d1131
VitorColombo <vitorcolombonunes@gmail.com>
2025-01-17 17:58:53 -0300

    fix: ajuste header tela de meus registros

--- lib/pages/my_registers.dart
+++ lib/pages/my_registers.dart
@@ -70,98 +70,127 @@ class _MyRegistersState extends State<MyRegisters> {
     final displayRegisters = isLoading ? placeholderRegisters : registers;
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            PageHeader(
-                text: "Meus registros",
-                icon: const Icon(Icons.arrow_back),
-                onTap: () => widget.updateIndex(0)),
-            Padding(
-              padding: const EdgeInsets.only(top: 0, left: 20.0, bottom: 10),
-              child: Row(
-                children: [
-                  Icon(PhosphorIcons.funnel(), color: Colors.grey[500], size: 16),
-                  Text("Filtrar:", style: TextStyle(color: Colors.grey[500])),
-                  const SizedBox(width: 10),
-                  InkWell(
-                    onTap: () {
-                      fetchRegisters("Todos");
-                    },
-                    child: StatusLabel(
-                      status: "Todos",
-                      borderColor: selectedFilter == "Todos"
-                          ? Colors.blue
-                          : Colors.transparent,
+      body: CustomScrollView(
+        slivers: [
+SliverAppBar(
+            automaticallyImplyLeading: false,
+            pinned: true,
+            collapsedHeight: 115,
+            expandedHeight: 115,
+            backgroundColor: Colors.white,
+            shadowColor: Color.fromARGB(0, 173, 145, 145),
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                color: Colors.white,
+                child: Stack(
+                  children: [
+                    PageHeader(
+                        text: "Meus registros",
+                        icon: const Icon(Icons.arrow_back),
+                        onTap: () => widget.updateIndex(0)
                     ),
-                  ),
-                  const SizedBox(width: 10),
-                  InkWell(
-                    onTap: () {
-                      if (selectedFilter == "Validado") {
-                        fetchRegisters("Todos");
-                      } else {
-                        fetchRegisters("Validado");
-                      }
-                    },
-                    child: StatusLabel(
-                      status: "Validado",
-                      borderColor: selectedFilter == "Validado"
-                          ? Colors.blue
-                          : Colors.transparent,
+                    Padding(
+                      padding: const EdgeInsets.only(top: 110, left: 20.0, bottom: 10),
+                      child: Row(
+                        children: [
+                          Icon(PhosphorIcons.funnel(), color: Colors.grey[500], size: 16),
+                          Text("Filtrar:", style: TextStyle(color: Colors.grey[500])),
+                          const SizedBox(width: 10),
+                          InkWell(
+                            onTap: () {
+                              fetchRegisters("Todos");
+                            },
+                            child: StatusLabel(
+                              status: "Todos",
+                              borderColor: selectedFilter == "Todos"
+                                  ? Colors.blue
+                                  : Colors.transparent,
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          InkWell(
+                            onTap: () {
+                              if (selectedFilter == "Validado") {
+                                fetchRegisters("Todos");
+                              } else {
+                                fetchRegisters("Validado");
+                              }
+                            },
+                            child: StatusLabel(
+                              status: "Validado",
+                              borderColor: selectedFilter == "Validado"
+                                  ? Colors.blue
+                                  : Colors.transparent,
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          InkWell(
+                            onTap: () {
+                              if (selectedFilter == "Enviado") {
+                                fetchRegisters("Todos");
+                              } else {
+                                fetchRegisters("Enviado");
+                              }
+                            },
+                            child: StatusLabel(
+                              status: "Enviado",
+                              borderColor: selectedFilter == "Enviado"
+                                  ? Colors.blue
+                                  : Colors.transparent,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 10),
-                  InkWell(
-                    onTap: () {
-                      if (selectedFilter == "Enviado") {
-                        fetchRegisters("Todos");
-                      } else {
-                        fetchRegisters("Enviado");
-                      }
-                    },
-                    child: StatusLabel(
-                      status: "Enviado",
-                      borderColor: selectedFilter == "Enviado"
-                          ? Colors.blue
-                          : Colors.transparent,
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-            !isLoading && registers.isEmpty
-                ? Center(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text(
-                        "Nenhum registro encontrado.",
-                        style: const TextStyle(fontSize: 18),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  )
-                : SizedBox(
-                    height: MediaQuery.of(context).size.height - kToolbarHeight,
-                    child: Skeletonizer(
-                      enabled: isLoading,
-                      child: ListView.builder(
-                        padding: const EdgeInsets.only(top: 0, bottom: 180),
-                        physics: const AlwaysScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: displayRegisters.length,
-                        itemBuilder: (context, index) {
-                          return RegisterItem(
-                            register: displayRegisters[index],
-                            route: RegisterDetailPage.routeName,
-                            isLoading: isLoading,
-                          );
-                        },
-                      ),
-                    ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      !isLoading && registers.isEmpty
+                          ? Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(
+                                  "Nenhum registro encontrado.",
+                                  style: const TextStyle(fontSize: 18),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            )
+                          : SizedBox(
+                              height: MediaQuery.of(context).size.height - kToolbarHeight,
+                              child: Skeletonizer(
+                                enabled: isLoading,
+                                child: ListView.builder(
+                                  padding: const EdgeInsets.only(top: 0, bottom: 200),
+                                  physics: const AlwaysScrollableScrollPhysics(),
+                                  shrinkWrap: true,
+                                  itemCount: displayRegisters.length,
+                                  itemBuilder: (context, index) {
+                                    return RegisterItem(
+                                      register: displayRegisters[index],
+                                      route: RegisterDetailPage.routeName,
+                                      isLoading: isLoading,
+                                    );
+                                  },
+                                ),
+                              ),
+                            ),
+                    ],
                   ),
-          ],
-        ),
+                ),
+              ]
+            )
+          )
+        ],
       ),
     );
   }
